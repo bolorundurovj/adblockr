4bfca003630d72171d4e0e70076501653a70bb36
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-12-28 16:08:29 +0100

    VJ Adblocker

---
+++ background.js
@@ -0,0 +1,24 @@
+const defaultFilters = [
+  '*://*.zedo.com/*',
+  '*://*.googleadservices.com/*',
+  '*://*.doubleclick.net/*',
+  '*://*.adskeeper.com/*',
+  '*://*.mellowads.com/*',
+  '*://*.googlesyndication.com/*',
+  '*://*.adbrite.com/*',
+  '*://*.quantserve.com/*',
+  '*://*.scorecardresearch.com/*',
+  '*://*.exponential.com/*',
+  '*://*.creative.ak.fbcdn.net/*',
+  '*://*.google-analytics.com/*',
+];
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    return { cancel: true };
+  },
+  {
+    urls: defaultFilters,
+  },
+  ['blocking']
+);
